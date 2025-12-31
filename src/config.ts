58d2b039b9f5ad496c0b46db8c4a3e413d68407d
i5114514kf <xiweilu15@gmail.com>
2026-01-01 05:44:16 +0800

    feat(导航栏): 添加统计链接并启用Umami统计功能
    在导航栏配置中添加统计链接，指向Umami统计页面
    启用Umami统计功能并更新相关配置，包括API密钥和脚本地址

--- src/config.ts
+++ src/config.ts
@@ -280,6 +280,12 @@ export const navBarConfig: NavBarConfig = {
 				},
 			],
 		},
+		{
+			name: "统计",
+			url: "https://cloud.umami.is/analytics/us/share/k1rR4VXVvnK01ETV",
+			external: true,
+			icon: "material-symbols:link",
+		},
 	],
 };
 
@@ -599,11 +605,12 @@ export const widgetConfigs = {
 } as const;
 
 export const umamiConfig = {
-	enabled: false, // 是否显示Umami统计
-	apiKey: import.meta.env.UMAMI_API_KEY || "api_xxxxxxxx", // API密钥优先从环境变量读取，否则使用配置文件中的值
+	enabled: true, // 是否显示Umami统计
+	apiKey:
+		import.meta.env.UMAMI_API_KEY || "api_xG9TcXPeaDzfLq77LpuhuW3cNWjU9DUV", // API密钥优先从环境变量读取，否则使用配置文件中的值
 	baseUrl: "https://api.umami.is", // Umami Cloud API地址
 	scripts: `
-<script defer src="XXXX.XXX" data-website-id="ABCD1234"></script>
+<script defer src="https://cloud.umami.is/script.js" data-website-id="3ee239da-60a0-4a88-b1c4-49eb3bf80a3e"></script>
   `.trim(), // 上面填你要插入的Script,不用再去Layout中插入
 } as const;
 
